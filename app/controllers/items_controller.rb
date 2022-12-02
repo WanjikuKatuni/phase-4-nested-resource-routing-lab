8767d54d1db87a53b71fa3d9b9f96bf41be772b2
wanjiku <annritawanjiku77@gmail.com>
2022-12-03 00:02:12 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+    else
+      item = Item.create(item_params)
+    end 
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def not_found_response
+    render json: {error: "item is not found"}, status: :not_found
+  end
+
+
 
 end
